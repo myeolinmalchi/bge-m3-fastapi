a852d52df49f7cb8de88cedcad5804eea4788e48
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-10 03:13:13 +0900

    feat(cuda): override method `inference`

--- utils/onnxutils/cuda.py
+++ utils/onnxutils/cuda.py
@@ -1,7 +1,10 @@
-from typing import List, overload
+from multiprocessing import cpu_count
+from typing import Any, List, Optional
 import onnxruntime as ort
-
+from onnxruntime.capi.onnxruntime_inference_collection import InferenceSession
+from transformers import AutoTokenizer
 from schemas.embed import EmbedResult
+
 from .base import ONNXRuntime
 
 
@@ -33,12 +36,61 @@ class ONNXCudaRuntime(ONNXRuntime):
 
         return tokenizers, sessions
 
+    def inference(
         self,
+        queries: List[str],
+        session: Optional[InferenceSession] = None,
+        tokenizer: Optional[Any] = None,
     ):
+        if len(queries) > self.batch_size:
+            raise Exception(
+                f"최대 배치 크기를 초과한 입력({len(queries)} > {self.max_workers})입니다."
+            )
 
+        session = self.session if session is None else session
+        tokenizer = self.tokenizer if tokenizer is None else tokenizer
 
+        inputs = self.tokenizer(
+            queries,
+            padding="longest",
+            return_tensors="np",
+            truncation=True,
+        )
 
+        io_binding = session.io_binding()
+        for k, v in inputs.items():
+            ortvalue = ort.OrtValue.ortvalue_from_numpy(v, self.device_type, 0)
+            io_binding.bind_input(
+                name=k,
+                device_type=self.device_type,
+                device_id=self.device_id,
+                element_type=v.dtype,
+                shape=ortvalue.shape(),
+                buffer_ptr=ortvalue.data_ptr(),
+            )
 
+        output_names = ["dense_vecs", "sparse_vecs", "colbert_vecs"]
+        for output_name in output_names:
+            io_binding.bind_output(
+                name=output_name,
+                device_type=self.device_type,
+                device_id=self.device_id,
+            )
 
+        session.run_with_iobinding(io_binding)
+        outputs = io_binding.get_outputs()
 
+        dense_outputs = outputs[0].numpy()
+        sparse_outputs = [
+            {i: w for i, w in zip(indicies, weights)}
+            for indicies, weights in zip(
+                inputs["input_ids"], outputs[1].numpy().squeeze(-1)
+            )
+        ]
 
+        results = [
+            EmbedResult(dense=dense, sparse=sparse, chunk=query)
+            for dense, sparse, query in zip(dense_outputs, sparse_outputs, queries)
+        ]
+
+        return results
