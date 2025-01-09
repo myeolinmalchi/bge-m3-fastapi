016f760167f636e2d1c715be913f1827e3ad4802
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-10 03:22:31 +0900

    feat(cpu): override method `_init_tokenizer_session_pool`

--- utils/onnxutils/cpu.py
+++ utils/onnxutils/cpu.py
@@ -11,6 +11,23 @@ from transformers import AutoTokenizer
 
 class ONNXCpuRuntime(ONNXRuntime):
 
+    def _init_tokenizer_session_pool(self):
+        sess_options = ort.SessionOptions()
+        sess_options.execution_mode = ort.ExecutionMode.ORT_PARALLEL
+        sess_options.intra_op_num_threads = cpu_count() // 2
+        sess_options.inter_op_num_threads = cpu_count()
+
+        session = ort.InferenceSession(
+            self.model_path,
+            providers=["CPUExecutionProvider"],
+            sess_options=sess_options,
+        )
+
+        tokenizers = [
+            AutoTokenizer.from_pretrained(self.tokenizer_path)
+            for _ in range(self.max_workers)
         ]
+
+        return tokenizers, [session]
 
         )
