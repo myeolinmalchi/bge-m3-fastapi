498a48cafd6fbbb9fad7271910ccd762f63419e0
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-10 03:12:47 +0900

    feat(cuda): override method `_init_tokenizer_session_pool`
    - 세션과 토크나이저 인스턴스는 `max_workers`만큼 생성
    - 다중 처리시 cpu 코어 수 절반까지 스레드 생성

--- utils/onnxutils/cuda.py
+++ utils/onnxutils/cuda.py
@@ -12,6 +12,27 @@ class ONNXCudaRuntime(ONNXRuntime):
         self.device_type = "cuda"
         self.device_id = 0
 
+    def _init_tokenizer_session_pool(self):
+        sess_options = ort.SessionOptions()
+        sess_options.inter_op_num_threads = cpu_count() // 2
+        providers = [("CUDAExecutionProvider", {"device_id": 0})]
+
+        sessions = [
+            ort.InferenceSession(
+                self.model_path,
+                providers=providers,
+                sess_options=sess_options,
+            )
+            for _ in range(self.max_workers)
+        ]
+
+        tokenizers = [
+            AutoTokenizer.from_pretrained(self.tokenizer_path, use_fast=True)
+            for _ in range(self.max_workers)
+        ]
+
+        return tokenizers, sessions
+
         self,
     ):
 
