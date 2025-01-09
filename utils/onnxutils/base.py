0714101528984f8f404cf4631636c28c575ababa
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-10 02:44:16 +0900

    feat(base): property 및 생성자 메서드 변경
    - `ThreadPoolExecutor` 사용해서 멀티스레딩 및 비동기 처리 구현
    - 토크나이저 및 onnx 세션 초기화 로직 분리(`_init_tokenizer_session_pool`)
    - `tokenizer`, `session` property: 라운드로빈 방식으로 하나의 인스터스 반환

--- utils/onnxutils/base.py
+++ utils/onnxutils/base.py
@@ -1,9 +1,9 @@
-import threading
-from typing import List, Literal, overload
+from abc import abstractmethod
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any, List, Literal, Optional, Tuple
 
 from onnxruntime import InferenceSession
-import onnxruntime as ort
-from transformers import AutoTokenizer
+import asyncio
 
 from schemas.embed import EmbedResult
 
@@ -12,22 +12,53 @@ class ONNXRuntime:
     _instance = None
 
     def __new__(cls, *args, **kwargs):
+        if cls == ONNXRuntime:
+            raise Exception("ONNXRuntime 인스턴스는 직접 생성할 수 없습니다.")
+
         if ONNXRuntime._instance is None:
             print("New ONNXRuntime instance has been created")
             ONNXRuntime._instance = super().__new__(cls)
-        assert ONNXRuntime._instance is not None
+
         return ONNXRuntime._instance
 
     def __init__(
         self,
         tokenizer_path: str,
         model_path: str,
-        N: int = 1,
+        batch_size: int = 1,
+        max_workers: int = 1,
     ):
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        self.status_event = threading.Event()
-        self.status_event.clear()
-        self.N = N
+        self.batch_size = batch_size
+        self.max_workers = max_workers
+        self._pool = ThreadPoolExecutor(max_workers=max_workers)
+
+        self.model_path = model_path
+        self.tokenizer_path = tokenizer_path
+
+        temp = self._init_tokenizer_session_pool()
+        self._tokenizers = temp[0]
+        self._sessions = temp[1]
+
+        self._session_index = 0
+        self._tokenizer_index = 0
+
+    @abstractmethod
+    def _init_tokenizer_session_pool(self) -> Tuple[List, List[InferenceSession]]:
+        raise NotImplementedError(
+            "method '_init_tokenizer_session_pool' must be implemented"
+        )
+
+    @property
+    def session(self) -> InferenceSession:
+        session = self._sessions[self._session_index % self.max_workers]
+        self._session_index += 1
+        return session
+
+    @property
+    def tokenizer(self):
+        tokenizer = self._tokenizers[self._tokenizer_index % self.max_workers]
+        self._tokenizer_index += 1
+        return tokenizer
 
     @classmethod
     def get_runtime(cls):
