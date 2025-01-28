807690e4780e532739a57cfbb3187c2a0275f3f3
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-28 23:16:22 +0900

    feat: 모델 런타임 초기화 함수 개선

--- utils/embed/__init__.py
+++ utils/embed/__init__.py
@@ -7,22 +7,37 @@ from utils.logger import _logger
 
 logger = _logger(__name__)
 
+_runtime = None
+
 
 def init_runtime(
-    model_path: str,
+    model_path: Optional[str] = None,
     tokenizer_path: Optional[str] = None,
     batch_size: int = 1,
     max_workers: int = 1,
     backend: Literal["onnx", "llama_cpp"] = "onnx",
     device: Literal["cpu", "cuda"] = "cpu",
 ):
-    match backend:
-        case "onnx":
-            Embedder = ONNXCpuRuntime if device == "cpu" else ONNXCudaRuntime
-            return Embedder(model_path, tokenizer_path, batch_size, max_workers)
-        case "llama_cpp":
-            if device == "cuda":
-                logger(
-                    "'llama_cpp' backend supports cpu only.", level="warning"
+    global _runtime
+
+    if _runtime is None:
+        if not model_path:
+            raise ValueError("model_path is required")
+
+        match backend:
+            case "onnx":
+                Embedder = ONNXCpuRuntime if device == "cpu" else ONNXCudaRuntime
+                _runtime = Embedder(
+                    model_path, tokenizer_path, batch_size, max_workers
                 )
-            return LlamaCppEmbedder(model_path)
+            case "llama_cpp":
+                if device == "cuda":
+                    logger(
+                        "'llama_cpp' backend supports cpu only.",
+                        level="warning"
+                    )
+                _runtime = LlamaCppEmbedder(
+                    model_path, batch_size=batch_size, max_workers=max_workers
+                )
+
+    return _runtime
