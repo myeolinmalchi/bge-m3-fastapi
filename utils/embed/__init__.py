4e25ed68d1689d7fb9ab4b29676343a19ea6f08e
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-02-16 18:50:36 +0900

    feat: `init_runtime` 리팩터링

--- utils/embed/__init__.py
+++ utils/embed/__init__.py
@@ -1,4 +1,5 @@
 from typing import Literal, Optional
+from utils.embed.base import AbsEmbedder
 from utils.logger import _logger
 
 logger = _logger(__name__)
@@ -12,33 +13,50 @@ def init_runtime(
     sessions: int = 1,
     backend: Literal["onnx", "llama_cpp"] = "onnx",
     device: Literal["cpu", "cuda"] = "cpu",
-):
+) -> AbsEmbedder:
     global _runtime
 
-    if _runtime is None:
-        if not model_path:
-            raise ValueError("model_path is required")
+    if _runtime:
+        return _runtime
 
-        match backend:
-            case "onnx":
-                from .onnx import ONNXCpuRuntime, ONNXCudaRuntime
-                Embedder = ONNXCpuRuntime if device == "cpu" else ONNXCudaRuntime
-                tokenizer_path = tokenizer_path if tokenizer_path else "BAAI/bge-m3"
-                _runtime = Embedder(
-                    model_path,
-                    tokenizer_path,
-                    batch_size,
-                    sessions,
+    if not model_path:
+        raise ValueError("model_path is required")
+
+    match (backend, device):
+        case ("onnx", "cpu"):
+            from .onnx import ONNXCpuRuntime
+            tokenizer_path = tokenizer_path if tokenizer_path else "BAAI/bge-m3"
+            _runtime = ONNXCpuRuntime(
+                model_path,
+                tokenizer_path,
+                batch_size,
+                sessions,
+            )
+
+        case ("onnx", "cuda"):
+            from .onnx import ONNXCudaRuntime
+            tokenizer_path = tokenizer_path if tokenizer_path else "BAAI/bge-m3"
+            _runtime = ONNXCudaRuntime(
+                model_path,
+                tokenizer_path,
+                batch_size,
+                sessions,
+            )
+
+        case ("llama_cpp", "cpu"):
+            from .llama_cpp import LlamaCppEmbedder
+            _runtime = LlamaCppEmbedder(
+                model_path, batch_size=batch_size, max_workers=sessions
+            )
+
+        case ("llama_cpp", "cuda"):
+            from .llama_cpp import LlamaCppEmbedder
+            if device == "cuda":
+                logger(
+                    "'llama_cpp' backend supports cpu only.", level="warning"
                 )
-            case "llama_cpp":
-                from .llama_cpp import LlamaCppEmbedder
-                if device == "cuda":
-                    logger(
-                        "'llama_cpp' backend supports cpu only.",
-                        level="warning"
-                    )
-                _runtime = LlamaCppEmbedder(
-                    model_path, batch_size=batch_size, max_workers=sessions
-                )
+            _runtime = LlamaCppEmbedder(
+                model_path, batch_size=batch_size, max_workers=sessions
+            )
 
     return _runtime
