756db7e42a117dd8575c4d4ef60f9f5e71e83f64
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-02-16 18:50:17 +0900

    feat: 요청 스키마 수정 및 `main.py` 리팩터링

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import Depends, FastAPI, HTTPException
 from schemas.embed import EmbedRequest, EmbedResponse
+from utils.embed.base import AbsEmbedder
+from utils.preprocess.text import md
 from utils.text import preprocess, split_chunks
 from utils.embed import init_runtime
 import cli
-
-from tqdm import tqdm
-
-runtime = None
 
 app = FastAPI()
 
@@ -18,39 +16,49 @@ def heath_check():
 
 @app.post("/embed")
 async def embed(
-    req: EmbedRequest, runtime=Depends(init_runtime)
+    req: EmbedRequest, runtime: AbsEmbedder = Depends(init_runtime)
 ) -> EmbedResponse:
+    from itertools import chain, islice
+
     try:
-        inputs = req.inputs
-        if isinstance(inputs, list):
-            if not req.chunking:
-                inputs = preprocess(inputs)
-                results = await runtime.batch_inference_async(inputs)
-            else:
-                from itertools import chain, islice
+        match req:
+            case EmbedRequest(inputs=list(inputs), html=True):
+                markdowns = [md(input) for input in inputs]
+                results = await runtime.batch_inference_async(markdowns)
+                response = [[result] for result in results]
 
-                chunks = [
-                    split_chunks(i) for i in tqdm(inputs, desc="Chunking")
-                ]
-
-                chunks = [preprocess(chunk) for chunk in chunks]
+            case EmbedRequest(inputs=list(inputs), chunking=True, html=False):
+                chunks = [preprocess(chunk) for chunk in inputs]
                 lens = [len(_chunks) for _chunks in chunks]
                 flatten_chunks = list(chain(*chunks))
 
-                temp = await runtime.batch_inference_async(flatten_chunks)
-                iterator = iter(temp)
-                results = [list(islice(iterator, length)) for length in lens]
+                iterator = iter(
+                    await runtime.batch_inference_async(flatten_chunks)
+                )
 
-            return results
+                response = [list(islice(iterator, length)) for length in lens]
 
-        if not req.chunking:
-            cleaned = preprocess(inputs)
-            result = await runtime.batch_inference_async([cleaned])
-            return result[0]
-        chunks = split_chunks(inputs)
-        cleaned = preprocess(chunks)
-        results = await runtime.batch_inference_async(cleaned)
-        return results
+            case EmbedRequest(inputs=str(input), chunking=False):
+                cleaned = preprocess(input)
+
+                assert isinstance(cleaned, str)
+
+                result = await runtime.batch_inference_async([cleaned])
+                response = result[0]
+
+            case EmbedRequest(inputs=str(input), chunking=True):
+                chunks = split_chunks(input)
+                cleaned = preprocess(chunks)
+
+                assert isinstance(cleaned, list)
+
+                response = await runtime.batch_inference_async(cleaned)
+
+            case _:
+                response = []
+
+        return response
+
     except Exception as e:
         raise HTTPException(
             status_code=400, detail=f"Error has been occurred {e}"
